8a1e1a7c5c68c8d94a86a5f628cbd074b9c8cf3e
Patrick Eriksson <paleite@kth.se>
2017-07-05 11:45:20 +0200

    First version of script

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const Promise = require('bluebird')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const moment = require('moment')
+moment.locale('sv')
+
+const baseUrl = 'http://downdetector.se/problem-storningar/'
+const servicesList = [
+  'bankik', // BankID
+  'getswish',
+  'handelsbanken',
+  'nordea',
+  'seb',
+  'skandia',
+  'sparbanken-oresund',
+  'swedbank'
+]
+
+const prettyNames = {
+  bankik: 'BankID',
+  getswish: 'Swish',
+  handelsbanken: 'Handelsbanken',
+  nordea: 'Nordea',
+  seb: 'SEB',
+  skandia: 'Skandiabanken',
+  sparbanken: 'Sparbanken Öresund',
+  swedbank: 'Swedbank'
+}
+
+const output = {}
+
+Promise.map(servicesList, (serviceId) => {
+  const url = `${baseUrl}${serviceId}/storningar-oversikt/`
+  const events = []
+
+  return axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data)
+
+      $('.event').each((i, elm) => {
+        const eventDateString = $('h2 span.date', elm).text().trim()
+        const eventDate = moment(eventDateString, 'D MMM YYYY').format()
+        $('h2 span.date', elm).remove()
+        const title = $('h2', elm).text().trim()
+        const link = `http://downdetector.se${$('.content a', elm).attr('href')}`
+        $('.content a', elm).remove()
+        const description = $('.content', elm).text().trim()
+
+        events.push({
+          eventDate,
+          title,
+          link,
+          description
+        })
+      })
+
+      return events
+    })
+    .then((events) => {
+      output[serviceId] = {
+        name: prettyNames[serviceId],
+        events
+      }
+    })
+})
+  .then(() => fs.writeFileSync('shame.json', JSON.stringify(output, null, 2)))
+  .catch((reason) => console.error('Error:', reason))
