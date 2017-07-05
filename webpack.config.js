d7a6af65646d66a682943e5ee12df3b4c50a452f
Igor Eklund <igor.eklund@trustly.com>
2017-07-05 12:12:04 +0200

    Merge in shome shame

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
   output: {
     path: __dirname + "/dist",
     filename: "main.js"
+  },
+
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }]
   }
 };
